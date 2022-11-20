21331205a7b6a62c633129b499b445101121dcdb
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:22:01 +0300

    Add files via upload

---
+++ 9.89/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _9._89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст: ");
+            string a = Console.ReadLine();
+
+            a = a.Replace('е', 'и');
+
+            Console.WriteLine(a);
+        }
+    }
+}
